bdc7203d49f7a4924e41a92cb1316fa186dd5be4
Laurent Laborde <kerdezixe@gmail.com>
2022-08-16 13:57:13 +0200

    setup main structure

--- main.js
+++ main.js
@@ -7,3 +7,31 @@
 const mainCanvas = document.getElementById('mainCanvas');
 mainCanvas.height = window.innerHeight;
 mainCanvas.width = window.innerHeight;
+const mainCtx = mainCanvas.getContext('2d');
+
+// Start the program
+setup();
+
+// setup
+function setup() {
+    update(); // Call update at least once before calling it in a loop (may be removed in the future)
+    setInterval(update, 1000/60); // 60 fps (for now, so that it doesn't run too fast too debug)
+    requestAnimationFrame(draw);  // draw every frame
+}
+
+// update (potentially, and hopefully, many updates per drawn frame)
+// this is not fps independent, it should update as fast as possible regardless of fps
+function update() {
+
+}
+
+// draw
+function draw() {
+    // clear the canvas
+    mainCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+
+    // draw stuff
+
+    // call this function again next frame
+    requestAnimationFrame(draw);
+}
