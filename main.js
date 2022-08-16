004d4b5bcca64d747ffcde8da54a4fb6611f2d69
Laurent Laborde <kerdezixe@gmail.com>
2022-08-16 13:20:25 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+    Credits & stuff
+ */
+
+
+// Create & configure main canvas
+const mainCanvas = document.getElementById('mainCanvas');
+mainCanvas.height = window.innerHeight;
+mainCanvas.width = window.innerHeight;
